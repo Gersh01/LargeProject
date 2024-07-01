bff0ef9ba3f4a628bae7bc360916e40329f83c3e
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-06-30 20:33:18 -0400

    Wrote out Create function

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -5,8 +5,78 @@ import NavBar from "../components/nav/NavBar";
 import BubblePanel from "../components/reusable/BubblePanel";
 
 const CreatePage = () => {
+	
 	//TODO: Finish Creation Function
-	const doCreate = async (e) => {};
+	const doCreate = async (e) => {
+	const [projectTitle, setProjectTitle] = useState("");
+	const [startDate, setStartDate] = useState("");
+	const [deadline, setDeadline] = useState("");
+	const [description, setDescription] = useState("");
+	const [expectations, setExpectations] = useState("");
+	const [isOpen, setOpen] = useState(true);
+	const [isDone, setDone] = useSate(false);
+	const [isStarted, setIsStarted] = useState(false);
+	const [dateCreated, setDateCreated] = ("TO BE UPDATED");
+	const [ownerID, setOwnerID] = user._id;
+	const [currentVsRequired, setCurrentVsRequired] = useState();
+	const [technologies, setTechnologies] = useState([]);
+	const [roles, setRoles] = useState([]);
+
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  //TODO: Finish Creation Function
+  const doCreate = async (e) => {
+    setErrorMessage("");
+    if(
+      //TODO: Must correctly build these out    
+      projectTitle !== "" &&
+      startDate !== "" &&
+      deadline !== "" &&
+      description !== "" &&
+      expectations !== ""
+    ){
+      //TODO: May want to add Form validation (start date vs deadline checking, making sure roles are present, etc.)
+      const validProject = () => {
+
+      }
+      
+      //TODO: Function to calculate group size via counting of roles
+
+      const newProject = {
+        isOpen: isOpen,
+        isDone: isDone,
+        isStarted: isStarted,
+        dateCreated: dateCreated,
+        ownerID: ownerID,
+        currentVsRequired: currentVsRequired,
+        deadline: deadline,
+        projectStartDate: startDate,
+        roles: roles,
+        technologies: technologies,
+        title: projectTitle,
+        description: description
+      };
+
+      try{
+        const response = await Axios.post(
+          "http://localhost:5000/api/projects",
+          newProject
+        );
+        console.log(response.status);
+        if(response && response.status === 201) {
+          console.log("Positive response");
+          //TODO: Immediately Navigate to created project
+          navigate("/project");
+        }
+
+      } catch(err) {
+        //TODO: Create Error messages based on error messages from API
+        
+      }
+    };
+	};
 
 	return (
 		<div>
@@ -62,3 +132,5 @@ const CreatePage = () => {
 		</div>
 	);
 };
+
+export default CreatePage;
